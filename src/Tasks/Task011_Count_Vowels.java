193fd05913e0ee6ae0d8d0f61829ee4f9012c954
skrisH <skrishnan577@gmail.com>
2025-03-26 22:28:40 +0530

    26-Mar Commit

--- src/Tasks/Task011_Count_Vowels.java
+++ src/Tasks/Task011_Count_Vowels.java
@@ -1,4 +1,28 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task011_Count_Vowels {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String input = scanner.next().toLowerCase();
+
+        int vowels = 0;
+        int consonants = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') { // Check if it's a letter
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+
+        }
+        System.out.println("Vowels : " + vowels);
+        System.out.println("Consonants : " + consonants);
+    }
 }
